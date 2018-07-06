f695e779ca93630c6998bf304b51e1929821ba1f
Nathan Tackett <Ntackett@veraxx.net>
2018-07-06 15:36:24 -0400

    Once again misunderstood tje problem. This is written for sub arrays, not subsequences

--- 2018/CodeChef/JulyChallenge2018/magicset.cpp
+++ 2018/CodeChef/JulyChallenge2018/magicset.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+bool isGoodSequence(int i1, int i2, vector<int> a, int m)
+{
+   if(i2 - i1 == a.size())
+      return false;
+      
+   int sum = 0;
+   for(int i = i1; i <= i2; i++)
+      sum += a[i];
+
+   return sum % m == 0;
+}
+
 int main()
 {
    int tc;
@@ -21,27 +33,43 @@ int main()
          a.push_back(x);
       }
 
+      // for(int i = 0; i < n; i++)
+      // {
+      //    //get sum from i to n-1
+      //    int sum = 0;
+      //    for(int j = i; j < n; j++)
+      //       sum += a[j];
+      //
+      //    if(sum % m == 0)
+      //    {
+      //       cout<<sum<<" mod "<<m<<" = 0"<<endl;
+      //       total++;
+      //    }
+      //
+      //    //first remove n - 1 and check the sum, then n - 2, then n - 3, and eventually i by itself
+      //    for(int j = n - 1; j > i; j--)
+      //    {
+      //       sum -= a[j];
+      //       if(sum % m == 0)
+      //       {
+      //          cout<<sum<<" mod "<<m<<" = 0"<<endl;
+      //          total++;
+      //       }
+      //    }
+      // }
+
       for(int i = 0; i < n; i++)
       {
-         //get sum from i to n-1
-         int sum = 0;
          for(int j = i; j < n; j++)
-            sum += a[j];
-
-         if(sum % m == 0)
          {
-            cout<<sum<<" mod "<<m<<" = 0"<<endl;
-            total++;
-         }
-
-         //first remove n - 1 and check the sum, then n - 2, then n - 3, and eventually i by itself
-         for(int j = n - 1; j > i; j--)
-         {
-            sum -= a[j];
-            if(sum % m == 0)
+            //is good sequence?
+            if(isGoodSequence(i, j, a, m))
             {
-               cout<<sum<<" mod "<<m<<" = 0"<<endl;
                total++;
+            }
+            else
+            {
+               break;
             }
          }
       }
